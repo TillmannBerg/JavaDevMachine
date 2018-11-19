9749fc25e38770901598553fb29ccb8be1b4a4a8
Tillmann Berg <TillmannBerg@users.noreply.github.com>
2018-11-19 12:33:06 -0800

    Add Ansible playbook to install packages

--- setup.sh
+++ setup.sh
@@ -3,3 +3,5 @@ apt install -y software-properties-common
 apt-add-repository ppa:ansible/ansible
 apt update
 apt install -y ansible
+
+ansible-pull -U https://github.com/TillmannBerg/JavaDevMachine.git
